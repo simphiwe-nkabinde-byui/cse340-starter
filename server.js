1bb8f0661be0ca36c2c01558cd7bbee2cb766217
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-05-07 00:19:57 +0200

    serve index view

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ const static = require("./routes/static")
  *************************/
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
-app.set('layout', '.layouts/layout') // not at views root
+app.set('layout', './layouts/layout') // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
