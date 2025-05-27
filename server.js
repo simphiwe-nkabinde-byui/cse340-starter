9429138801d2bf51c71f6c6ad0f82a67149b4610
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-05-27 11:26:46 +0200

    W03 Learning Activities: Content Devlivery & MVC pattern

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require('express-ejs-layouts')
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
@@ -23,9 +25,8 @@ app.set('layout', './layouts/layout') // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
