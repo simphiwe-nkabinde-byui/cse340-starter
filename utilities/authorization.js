8cda82e5f8658cc75a1489f23a9199ec5f1f60a3
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-06-16 16:28:17 +0200

    Assignment 5: Account Management - Task 3

--- utilities/authorization.js
+++ utilities/authorization.js
@@ -1,12 +1,12 @@
 async function AdminEmployeeOnly(req, res, next) {
   if (res.locals.loggedin) {
-    const account_type = res.locals.accountData.type;
-    console.log({ account_type });
-    next();
-  } else {
-    req.flash("notice", "You need to be logged in as an admin or employee to access the resource.");
-    return res.redirect("/account/login");
+    const { account_type } = res.locals.accountData;
+    if (account_type == "Admin" || account_type == "Employeee") {
+      return next();
+    }
   }
+  req.flash("notice", "You need to be logged in as an admin or employee to access the resource.");
+  return res.redirect("/account/login");
 }
 
-module.exports = { AdminEmployeeOnly }
+module.exports = { AdminEmployeeOnly };
